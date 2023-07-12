dcc037643be1dc2df9fab63e082c078fb2cb74b8
MPMAHITHSAI <mpmahithsai@gmail.com>
2023-07-12 16:38:05 +0530

    array swap

---
+++ Rangerev.java
@@ -0,0 +1,37 @@
+package Revrange;
+
+import java.util.Scanner;
+
+class Arrayprob{
+	public static int[] verify(int A[]) 
+	{
+		Scanner	sc = new Scanner(System.in);
+	int size = A.length;
+	int B = sc.nextInt();
+	int C = sc.nextInt();
+	while(B<C) {
+		int temp = A[B];
+		A[B]=A[C];
+		A[C]=temp;
+		B++;C--;
+	}
+	return A;
+	}
+}
+public class Rangerev{
+	public static void main (String[] args) {
+	Scanner	sc = new Scanner(System.in);
+	int n = sc.nextInt();
+	int arr[] = new int [n];
+	for (int i=0;i<arr.length;i++) {
+		arr[i] = sc.nextInt();
+	}
+	Arrayprob Array = new Arrayprob();
+	int pair[]=Arrayprob.verify(arr);
+	for(int i=0;i<pair.length;i++) {
+		System.out.print(pair[i]);
+	}
+	
+	
+	}
+}
